0aac1d12409c459ea608dc25a8e1be63835a55eb
João Victor da Silva Sales <56794256+VictorSilvaaa@users.noreply.github.com>
2020-11-11 16:43:45 -0300

    Update Landing.tsx

--- web/frontEnd/src/pages/Landing.tsx
+++ web/frontEnd/src/pages/Landing.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiArrowRight } from 'react-icons/fi';
 import '../styles/global.css';
 import '../styles/pages/landing.css';
 import logoImg from '../images/logoHelping.svg';
+import homeimg from '../images/landingHelping.svg';
 
 function landing() {
     return (
       <div id="page-landing">
         <div className="content-wrapper"> 
-          <img src= { logoImg } alt="Happy"></img>
+          <img src= { logoImg } alt="logoHelping"></img>
+          
+          
    
             <h1>Faça uma doação e salve vidas. Uma gota pode fazer a diferença</h1>
-  
+         
           <div className="location">
             <strong>São Luís</strong>
             <span>Maranhão</span>

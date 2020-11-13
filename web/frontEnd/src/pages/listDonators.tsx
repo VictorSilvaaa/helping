c2412ae4e770deec81aa8cd8b19d4023b335c2ca
João Victor da Silva Sales <56794256+VictorSilvaaa@users.noreply.github.com>
2020-11-13 11:47:27 -0300

    estrutura

---
+++ web/frontEnd/src/pages/listDonators.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "../styles/pages/listDonators.css";
+
+import PageHeader from "../components/PageHeader/";
+import TeacherItem from "../components/TeacherItem";
+import Input from "../components/Input";
+import Select from "../components/Select";
+
+export default function TeacherList() {
+  return (
+    <div className="container" id="page-teacher-list">
+      <PageHeader title="Esses são os Proffys disponíveis.">
+        <form id="search-teachers">
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "Matematica", label: "Matematica" },
+              { value: "Ingles", label: "Ingles" },
+              { value: "Historia", label: "Historia" },
+              { value: "Fisica", label: "Fisica" },
+              { value: "Portugues", label: "Portugues" },
+              { value: "Quimica", label: "Quimica" },
+            ]}
+          />
+          <Select
+            name="week_day"
+            label="Dia da Semana"
+            options={[
+              { value: "0", label: "Domingo" },
+              { value: "1", label: "Segunda" },
+              { value: "2", label: "Terça" },
+              { value: "3", label: "Quarta" },
+              { value: "4", label: "Quinta" },
+              { value: "5", label: "Sexta" },
+              { value: "6", label: "Sábado" },
+            ]}
+          />
+
+          <Input name="time" label="Hora" type="time" />
+        </form>
+      </PageHeader>
+
+      <main>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </main>
+    </div>
+  );
+}

c4bf490d8cdc942db59577f768e4d56dba64ede7
João Victor da Silva Sales <56794256+VictorSilvaaa@users.noreply.github.com>
2020-11-14 12:01:36 -0300

    api start

--- web/frontEnd/src/pages/TeacherForm/index.tsx
+++ web/frontEnd/src/pages/TeacherForm/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { ReactElement, useState, FormEvent } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import "./styles.css";
 import PageHeader from "../../components/PageHeader/";
@@ -7,8 +8,64 @@ import Textarea from "../../components/Textarea/";
 import Select from "../../components/Select/";
 
 import warningIcon from "../../assets/images/icons/warning.svg";
+import api from '../../services/api';
 
-export default function createDonator() {
+
+function createDonator(): ReactElement {
+  const history = useHistory();
+
+  const [name, setName] = useState('');
+  const [avatar, setAvatar] = useState('');
+  const [bio, setBio] = useState('');
+  const [whatsapp, setWhatsapp] = useState('');
+
+  const [subject, setSubject] = useState('');
+  const [cost, setCost] = useState('');
+
+  const [scheduleItems, setScheduleItems] = useState([
+    { week_day: 0, from: '', to: '' },
+  ]);
+
+  function addNewScheduleItem() {
+    setScheduleItems([...scheduleItems,
+    { week_day: 0, from: '', to: '' },
+    ]);
+  }
+
+  function handleCreateClass(e: FormEvent) {
+    e.preventDefault();
+
+    api.post('classes', {
+      name,
+      avatar,
+      whatsapp,
+      bio,
+      subject,
+      cost: Number(cost),
+      schedule: scheduleItems,
+    }).then(() => {
+      alert('Cadastro realizado com sucesso!');
+
+      history.push('/');
+    }).catch(() => {
+      alert('Erro no cadastro.');
+    });
+  }
+
+  function setScheduleItemValue(position: number, field: string, value: string) {
+    const updateScheduleItems = scheduleItems.map((scheduleItem, index) => {
+      if (index === position) {
+        return { ...scheduleItem, [field]: value };
+      }
+
+      return scheduleItem;
+    });
+
+    setScheduleItems(updateScheduleItems);
+  }
+
+
+ 
   return (
     <div className="container" id="page-teacher-form">
       <PageHeader
@@ -17,38 +74,52 @@ export default function createDonator() {
       />
 
       <main>
-        <fieldset>
-          <legend>Seus Dados</legend>
-          <Input name="name" label="Nome Completo" />
-          <Input name="whatsapp"  label="Whatsapp" />
-          <Textarea name="description" label="Descrição" />
-        </fieldset>
+        <form>
+          <fieldset>
+            <legend>Seus Dados</legend>
+            <Input 
+              name="name" 
+              label="Nome Completo" 
+              onChange={(e) => { setName(e.target.value) }}
+            />
+            <Input name="whatsapp"  label="Whatsapp" />
+            <Textarea name="description" label="Descrição"
+            onChange={(e) => { setName(e.target.value) }}
+            />
+          </fieldset>
 
-        <fieldset>
-          <legend>Sobre</legend>
-          <Select
-            name="Blood-code"
-            label="Tipo sanguíneo"
-            options={[
-              { value: "A+", label: "A+" },
-              { value: "A-", label: "A-" },
-              { value: "B+", label: "B+" },
-              { value: "B-", label: "B-" },
-              { value: "AB+", label: "AB+" },
-              { value: "AB-", label: "AB-" },
-            ]}
-          />
-        </fieldset>
+          <fieldset>
+            <legend>Sobre</legend>
+            <Select
+              name="Blood-code"
+              label="Tipo sanguíneo"
+              value={subject}
+              onChange={(e) => { setSubject(e.target.value) }}
+              options={[
+                { value: "A+", label: "A+" },
+                { value: "A-", label: "A-" },
+                { value: "B+", label: "B+" },
+                { value: "B-", label: "B-" },
+                { value: "AB+", label: "AB+" },
+                { value: "AB-", label: "AB-" },
+                
+              ]}
+            />
+          </fieldset>
 
-        <footer>
-          <p>
-            <img src={warningIcon} alt="Aviso Importante" />
-            Importante <br />
-            Preencha todos os dados
-          </p>
-          <button type="button">Salvar cadastro</button>
-        </footer>
+          <footer>
+            <p>
+              <img src={warningIcon} alt="Aviso Importante" />
+              Importante <br />
+              Preencha todos os dados
+            </p>
+            <button type="button">Salvar cadastro</button>
+          </footer>
+
+        </form>
+        
       </main>
     </div>
   );
 }
+export default createDonator;

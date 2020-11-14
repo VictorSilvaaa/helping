7274c4d52bf5ca03a5a80b8d6ab1b5533a503d1d
João Victor da Silva Sales <56794256+VictorSilvaaa@users.noreply.github.com>
2020-11-13 23:00:53 -0300

    home 1

--- web/frontEnd/src/pages/Landing/Landing.tsx
+++ web/frontEnd/src/pages/Landing/Landing.tsx
@@ -2,43 +2,37 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './landing.css';
+import './global.css';
 
 import iconBlood from '../../images/icons/blood-home.svg';
 import cadastro from '../../images/icons/cadastro.svg';
 
 import logoImg from '../../images/logoHelping.svg';
-import homeimg from '../../images/landingHelping.svg';
 
 function Landing() {
  
 
   return (
-
+    
     <div id="page-landing">
-      <div id="page-landing-content" className="container">
-        <div className="logo-container">
-          <img src={logoImg} alt="Helping" />
-          <h2>Faça uma doação e salve vidas. Uma gota pode fazer a diferença</h2>
-        </div>
+      <div id="content-wrapper">    
+          <div className="logo-container">
+              <img src={logoImg} alt="Helping" />
+           </div>
 
-        <img 
-          src={homeimg} 
-          alt="bloodHome" 
-          className="hero-image"
-        />
+           <h2>Faça uma doação e salve vidas. Uma gota pode fazer a diferença</h2>
 
-        <div className="buttons-container">
-          <Link to="/cadastro" className="cadastre-se">
-          <img src={cadastro} alt="Doadores"/>
-            Cadastre-se
-          </Link>
-
-          <Link to="/doadores" className="doadores">
-            <img src={iconBlood} alt="Doadores"/>
-            Doadores
-          </Link>
-        </div>
-      </div>
+           <div className="buttons-container">
+              <Link to="/cadastro" className="cadastre-se">
+                <img src={cadastro} alt="Doadores"/>
+                Cadastre-se
+              </Link>
+              <Link to="/doadores" className="doadores">
+                <img src={iconBlood} alt="Doadores"/>
+                Doadores
+               </Link>
+           </div>
+          </div>
     </div>
   )
 }

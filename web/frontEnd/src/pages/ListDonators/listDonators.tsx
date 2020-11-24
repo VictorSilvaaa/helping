96925d13c30d34d3a0e9782e59dea74e67e8af04
João Victor da Silva Sales <56794256+VictorSilvaaa@users.noreply.github.com>
2020-11-24 00:41:55 -0300

    gridList

--- web/frontEnd/src/pages/ListDonators/listDonators.tsx
+++ web/frontEnd/src/pages/ListDonators/listDonators.tsx
@@ -1,12 +1,17 @@
-import React ,{useState,FormEvent } from 'react';
+import React ,{useEffect,useState,FormEvent } from 'react';
 import api from '../../services/api';
+import './listDonators.css'
+import '../../assets/styles/global.css';
+import {FaUserAlt} from 'react-icons/fa';
+
+import PageHeader from "../../components/PageHeader";
 
 interface Donator{
   name: string;
   sex: string;
   phone_number: string;
   description: string;
-  blood_type:string;
+  blood:string;
   latitude:number;
   longitude:number;
 }
@@ -16,6 +21,11 @@ function ListDonators() {
 
 
   const [Donators, setDonators] = useState([]);
+  
+  useEffect(() => {
+    searchDonators()
+  },[])
+
   async function searchDonators() { 
   try{  
     const response = await api.get('/donators');
@@ -29,16 +39,42 @@ function ListDonators() {
   return (
     
     <div >
-      <h1>Listar doadores</h1>
-      <button type="button" onClick={searchDonators}>Search</button>
+        <PageHeader 
+        title="Aqui  estão alguns doadores disponíveis"
+        description="Entre em contato com algum doador e conversem"
+        >
+
+        </PageHeader>
       
       <main>
-        {Donators.map((donator:Donator) => {
-          return(
-            <div>
-              <p>{donator.name}</p>
+            <div id="listDonators">
+              <div className="cards">
+              {Donators.map((donator:Donator) => {
+                  return(
+                    <div className="card">
+                        <header>
+                            <FaUserAlt></FaUserAlt>
+                        </header>
+                        <section>
+                          <p>{donator.name}</p>
+                          <p>{donator.blood}</p>
+                          <p>{donator.description}</p>
+                          <button>
+                            <strong>Conhecer doador</strong>
+                          </button>
+                        </section>
+                       
+
+                        
+                        
+         
+                    </div>
+                  );
+              })}
+            
+              </div>
             </div>
-          )})}
+          
       </main>
     </div>
 

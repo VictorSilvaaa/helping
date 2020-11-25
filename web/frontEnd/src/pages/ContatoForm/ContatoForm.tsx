21f2a1de18618dd8ff2402a4fd8303c7369d86df
João Victor da Silva Sales <56794256+VictorSilvaaa@users.noreply.github.com>
2020-11-24 22:57:34 -0300

    cotactForm start

---
+++ web/frontEnd/src/pages/ContatoForm/ContatoForm.tsx
@@ -0,0 +1,16 @@
+import React from  'react';
+import PageHeader from "../../components/PageHeader";
+import { Link } from 'react-router-dom';
+
+function Contact(){
+    return (
+        <div>
+            <PageHeader 
+            src="/doadores"
+            title=""
+            description=""
+            ></PageHeader>
+        </div>
+    );
+}
+export default Contact;
--- web/frontEnd/src/routes.tsx
+++ web/frontEnd/src/routes.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Switch, Route} from 'react-router-dom';
 import Landing from './pages/Landing/Landing';
 import DonatorForm from './pages/DonatorForm/DonatorForm';
 import ListDonators from './pages/ListDonators/listDonators';
-
+import ContactForm from './pages/ContatoForm/ContatoForm';
 
 // exact -> o caminho precisa ser igual.
 // switch -> apenas uma rota irá ser chamada 
@@ -14,7 +14,8 @@ function Routes(){
            <Switch>
            <Route path="/" exact component={ Landing } />  
            <Route path="/doar"  component={ DonatorForm } />  
-           <Route path="/doadores"  component={ ListDonators } />  
+           <Route path="/doadores"  component={ ListDonators } />
+           <Route path="/contato/:id"  component={ ContactForm  } />   
            </Switch>          
         </BrowserRouter>
     );
